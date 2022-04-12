5b6a7366f5386c3c6002563109e5a90aab305a90
Leandro <leandronucci@outlook.com>
2022-04-12 13:24:49 -0300

    main

--- database/migrations/2022_04_12_161657_set_default_value_to_tag_from_users_table.php
+++ database/migrations/2022_04_12_161657_set_default_value_to_tag_from_users_table.php
@@ -27,7 +27,7 @@ class SetDefaultValueToTagFromUsersTable extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            
         });
     }
 }
